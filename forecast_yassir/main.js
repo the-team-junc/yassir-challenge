3baebea6f93da3d73c5c4d271cf20d8223471019
goldenfay <selmanefaycal@hotmail.fr>
2020-12-26 01:03:42 +0100

    Added setting handling + API request

--- forecast_yassir/main.js
+++ forecast_yassir/main.js
@@ -1,100 +1,59 @@
-window.addEventListener(
-    "load",
-    function () {
-        var form = document.getElementById("coords-form");
-    form.addEventListener(
-        "submit",
-        function (event) {
-            event.preventDefault();
-            if (form.checkValidity() === false) {
-                event.stopPropagation();
-            } else {
-              let SERVER_URL=document.getElementById('server-url-input').value;
-              let datastring=JSON.stringify($("#coords-form").serialize());
-              console.log(datastring)
-              console.log(datastring);
+window.addEventListener("load", function () {
+  var form = document.getElementById("coords-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      let SERVER_URL = document.getElementById("server-url-input").value;
+      let datastring = $("#coords-form").serialize();
+      //   let datastring=JSON.stringify($("#coords-form").serialize());
+      console.log(datastring);
+      console.log(datastring);
 
-            let requestOptions={
-                type: "POST",
-                url: SERVER_URL+"estimate",
-                // "Content-Type": "application/json",
-                // headers: {
-                //     'Access-Control-Allow-Origin': '*',
-                // },
-               
-                data: datastring
-              };
+      let requestOptions = {
+        type: "POST",
+        url: SERVER_URL + "estimate",
+        // "Content-Type": "application/json",
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        // },
 
+        data: datastring,
+      };
 
-              $.ajax({
-                ...requestOptions,
-                success: function (response) {
-                  console.log(response);
-                
-                },
-                error: function (error) {
-                  console.error(error);
-                  
-                }
-                
-
-                
-              });
-              
-
-
-          }
-        }
-    );
-
-
+      $.ajax({
+        ...requestOptions,
+        success: function (response) {
+          console.log(response);
+          $('#eta-container').append(`Estimated Travel Duration : ${response.eta}`)
+        },
+        error: function (error) {
+          console.error(error);
+        },
+      });
     }
+  });
+});
 
-);
+function request_estimation(data) {
+  let SERVER_URL = document.getElementById("server-url-input").value;
 
-function request_estimation(){
-    var form = document.getElementById("coords-form");
-    form.addEventListener(
-        "submit",
-        function (event) {
-            event.preventDefault();
-            if (form.checkValidity() === false) {
-                event.stopPropagation();
-            } else {
-              let SERVER_URL=document.getElementById('server-url-input').value;
-              let datastring=$("#coords-form").serialize();
+  let requestOptions = {
+    type: "POST",
+    url: SERVER_URL + "estimate",
 
-            let requestOptions={
-                type: "POST",
-                url: SERVER_URL+"estimate",
-               
-                data: datastring
-              };
+    data: data,
+  };
 
-
-              $.ajax({
-                ...requestOptions,
-                success: function (response) {
-                  console.log(response);
-                  displayLoading(false);
-                
-                },
-                error: function (error) {
-                  console.error(error);
-                  
-                }
-                
-
-                
-              });
-              
-
-
-          }
-        }
-    );
-
+  $.ajax({
+    ...requestOptions,
+    success: function (response) {
+      console.log(response);
+      $('#eta-container').append(`Estimated Travel Duration : ${response.eta}`)
+    },
+    error: function (error) {
+      console.error(error);
+    },
+  });
 }
-
-
-  
--- server.py
+++ server.py
@@ -2,22 +2,71 @@
 import pandas as pd
 from flask import Flask,render_template,request
 import numpy as np
-import os,time
+import os,time, datetime,traceback
+from geopy.distance import vincenty
+
+def distanceCalc(row):
+    '''calculate distance (km) between two lat&long points using the Vincenty formula '''
+
+    return vincenty((row.pickup_lat, row.pickup_lng), (row.dropoff_lat, row.dropoff_lng)).meters/1000.
+
+def distanceCalc(row):
+    '''calculate distance (km) between two lat&long points using the Vincenty formula '''
+
+    return vincenty((row.pickup_lat, row.pickup_lng), (row.dropoff_lat, row.dropoff_lng)).meters/1000.
+
+
+
+def construct_input_df(obj):
+    now=datetime.datetime.now()
+    date,hour,min=now.day,now.hour,now.minute
+    day_time_range=int(hour*4 + ( np.ceil(min/15) ) )
+    input_row=pd.DataFrame([obj])
+    input_row['distance']=input_row.apply(lambda x: distanceCalc(x), axis=1)
+    input_row['day_time_range'] =  input_row.apply(lambda x: day_time_range, axis=1)
+    input_row['date'] =  input_row.apply(lambda x: date, axis=1)
+
+    return input_row
+
+def make_prediction(model,pickup_lat,pickup_lng,dropoff_lat,dropoff_lng):
+   
+    input_raw={
+        "pickup_lat":pickup_lat,
+        "pickup_lng":pickup_lng,
+        "dropoff_lat":dropoff_lat,
+        "dropoff_lng":dropoff_lng
+
+    }
+    input_row=construct_input_df(input_raw)
+    res=float(model.predict(input_row)[0][0])
+    print(res,flush=True)
+    return {'eta':res }
 
 app = Flask(__name__)
-
 @app.route('/estimate',methods=['POST'])
 def estimate_ETA():
     params=request.values.to_dict()
     try:
         print(params,flush=True)
-        result=params
+        pickup_lat=float(params['depLat'])
+        pickup_lng=float(params['depLng'])
+        dropoff_lat=float(params['arrLat'])
+        dropoff_lng=float(params['arrLng'])
+        if 'token' in params.keys():
+            result={'eta': float(params['token'])}
     except Exception as e:
         return {'ok': False, 'message': 'Invalid params'},422,{'Access-Control-Allow-Origin': '*'}
+    try:
+      result=make_prediction(model,pickup_lat,pickup_lng,dropoff_lat,dropoff_lng)
+      print(result,flush=True)
+    except Exception as e: 
+      print('Error occured when predicting : ',flush=True)
+      traceback.print_exc()
+      
+      return with_header_response({'ok' : False, 'message': 'An error occured'},500)    
 
     
     return result,200,{'Access-Control-Allow-Origin': '*'}
-
 
  # For Access origin issues
 @app.after_request
